afefd92ce8b82081aa6134ee6e66c16da7799f20
Darcy McLean <darc.mclean@gmail.com>
2017-09-30 16:00:20 +0000

    Adds another functions exercise

---
+++ odd.js
@@ -0,0 +1,8 @@
+//20170930 DM - LHL prepwork, Module 4 [activity 29 (?)].
+
+function isOdd(num) {
+  return num % 2 !== 0;
+}
+
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
