37e845e5b2d5d45a4e2aa9394644537e12d27aae
Darcy McLean <darc.mclean@gmail.com>
2017-09-30 15:55:57 +0000

    Adds functions exercises

---
+++ even.js
@@ -0,0 +1,8 @@
+//20170930 DM - LHL prepwork, Module 4 [activity 29 (?)].
+
+function isEven(num) {
+  return num % 2 === 0;
+}
+
+console.log(isEven(10));
+console.log(isEven(11));
---
+++ hello.js
@@ -0,0 +1,9 @@
+//20170930 DM - LHL prepwork, Module 4 [activity 29 (?)].
+
+function sayHello(name) {
+  console.log("Hello, " + name);
+}
+
+sayHello("Caliban");
+sayHello("Miranda");
+sayHello("Ferdinand");
